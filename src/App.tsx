fa719543dcb66c98134c646a85a09d7e0c5586de
iamhansko <iamhansko@kakao.com>
2024-06-02 01:38:37 +0900

    chore: Disable Naver Map

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { NavermapsProvider } from 'react-naver-maps';
+// import { NavermapsProvider } from 'react-naver-maps';
 import { Heading1 } from '@/components/Text.tsx';
 import Wrapper from '@/components/Wrapper.tsx';
 import Account from '@/layout/Account/Account.tsx';
@@ -8,11 +8,11 @@ import FloatingBar from '@/layout/FloatingBar/FloatingBar.tsx';
 import GalleryWrap from '@/layout/Gallery/GalleryWrap.tsx';
 import Guestbook from '@/layout/Guestbook/Guestbook.tsx';
 import Invitation from '@/layout/Invitation/Invitation.tsx';
-import Location from '@/layout/Location/Location.tsx';
+// import Location from '@/layout/Location/Location.tsx';
 import Main from '@/layout/Main/Main.tsx';
 
 function App() {
-  const ncpClientId = import.meta.env.VITE_APP_NAVERMAPS_CLIENT_ID;
+  // const ncpClientId = import.meta.env.VITE_APP_NAVERMAPS_CLIENT_ID;
   const [isVisible, setIsVisible] = useState(false);
   const galleryRef = useRef(null);
 
@@ -37,7 +37,7 @@ function App() {
   };
 
   return (
-    <NavermapsProvider ncpClientId={ncpClientId}>
+    // <NavermapsProvider ncpClientId={ncpClientId}>
       <Container>
         <Wrapper>
           <Main />
@@ -54,17 +54,17 @@ function App() {
           <Heading1>마음 전하실 곳</Heading1>
           <Account />
         </Wrapper>
-        <Wrapper>
+        {/* <Wrapper>
           <Heading1>오시는 길</Heading1>
           <Location />
-        </Wrapper>
+        </Wrapper> */}
         <Wrapper>
           <Heading1>신랑 신부에게</Heading1>
           <Guestbook />
         </Wrapper>
         <FloatingBar isVisible={isVisible} />
       </Container>
-    </NavermapsProvider>
+    // </NavermapsProvider>
   );
 }
 
